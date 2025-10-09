36c7496e792b073c56f36078db64bc2a01cfd1d6
TVMOWYA <kyaged@gmail.com>
2025-10-09 00:40:13 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+ apiKey: "AIzaSyBPvb94wCfAdew9cY8Sph7Dwzq8JiK5Of4",
+
+  authDomain: "daily-dose-of-positivity-d8e78.firebaseapp.com",
+
+  projectId: "daily-dose-of-positivity-d8e78",
+
+  storageBucket: "daily-dose-of-positivity-d8e78.firebasestorage.app",
+
+  messagingSenderId: "873430812377",
+
+  appId: "1:873430812377:web:177cf4b6c2e48fc17d20c8"
